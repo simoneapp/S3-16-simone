af9710334db26e89f5d93a889853ee2ad8e3396c
sapi9 <michele.sapignoli3@studio.unibo.it>
2017-07-03 18:47:27 +0200

    Init Google Play API

--- app/src/main/java/app/simone/MainActivity.java
+++ app/src/main/java/app/simone/MainActivity.java
@@ -3,7 +3,10 @@ package app.simone;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -11,7 +14,16 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.Player;
 
+import app.simone.users.GoogleGamesActivity;
 import io.fabric.sdk.android.Fabric;
 
 import app.simone.users.FacebookLoginActivity;
@@ -19,14 +31,25 @@ import app.simone.users.FacebookLoginActivity;
 /**
  * @author Michele Sapignoli
  */
-public class MainActivity extends FullscreenActivity {
+public class MainActivity extends FullscreenActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
 
     private Button VSCpuButton;
     final Handler handler = new Handler();
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+         mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */,
+                        this /* OnConnectionFailedListener */)
+                .addApi(Games.API)
+                 .addConnectionCallbacks(this)
+                .addScope(Games.SCOPE_GAMES)
+                .build();
+
+
         Fabric.with(this, new Crashlytics());
 
         VSCpuButton = (Button)findViewById(R.id.button_vs_cpu);
@@ -46,6 +69,14 @@ public class MainActivity extends FullscreenActivity {
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.pulse);
         mainFab.startAnimation(animation);
         simoneTextView.startAnimation(animation);
+
+        mainFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GoogleGamesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -74,4 +105,18 @@ public class MainActivity extends FullscreenActivity {
         startActivity(intent);
     }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("##MAIN ACTIVITY", "Connection failed");
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
 }

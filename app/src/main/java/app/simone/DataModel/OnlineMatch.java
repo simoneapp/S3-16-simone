f996020a4eebb80f5b1f3464170d7d3f56a50203
Giacomo Bartoli <giacomo.bartoli3@studio.unibo.it>
2017-07-09 19:02:18 +0200

    massive refactoring due to multiplayer option

--- app/src/main/java/app/simone/DataModel/OnlineMatch.java
+++ app/src/main/java/app/simone/DataModel/OnlineMatch.java
@@ -1,5 +1,7 @@
 package app.simone.DataModel;
 
+import java.io.Serializable;
+
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 
@@ -7,16 +9,27 @@ import io.realm.annotations.PrimaryKey;
  * Created by Giacomo on 05/07/2017.
  */
 
-public class PendingRequest extends RealmObject{
+public class PendingRequest extends RealmObject implements Serializable {
 
     private String id;
     private String name;
+    private String scoreP1;
     @PrimaryKey
     private String idTo;
     private String nameTo;
+    private String scoreP2;
 
     public PendingRequest() {
 
+    }
+
+    public PendingRequest(String id, String name, String scoreP1,String idTo, String nameTo, String scoreP2) {
+        this.id=id;
+        this.name=name;
+        this.scoreP1=scoreP1;
+        this.idTo=idTo;
+        this.nameTo=nameTo;
+        this.scoreP2=scoreP2;
     }
 
     public String getId() {
@@ -51,5 +64,21 @@ public class PendingRequest extends RealmObject{
 
     public void setNameTo(String nameTo) {
         this.nameTo = nameTo;
+    }
+
+    public String getScoreP1() {
+        return scoreP1;
+    }
+
+    public void setScoreP1(String scoreP1) {
+        this.scoreP1 = scoreP1;
+    }
+
+    public String getScoreP2() {
+        return scoreP2;
+    }
+
+    public void setScoreP2(String scoreP2) {
+        this.scoreP2 = scoreP2;
     }
 }

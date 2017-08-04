3edc014508b239141a96a66e9c26bf177b3fcc18
gzano <giacomo.zanotti2@studio.unibo.it>
2017-08-04 21:00:59 +0200

    fake blinks work

--- app/src/main/java/app/simone/DistributedSimon/Activities/ColorSetUpActivity.java
+++ app/src/main/java/app/simone/DistributedSimon/Activities/ColorSetUpActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,13 +23,13 @@ import app.simone.R;
 public class ColorSetUpActivity extends AppCompatActivity {
 
 
-    private String playerID="";
-    private String color="";
+    private String playerID = "";
+    private String color = "";
 
     private DatabaseReference databaseReference;
     private final String CHILD_PLAYERS = "players";
 
-    private Button buttonColor,buttonInstantPlay;
+    private Button buttonColor, buttonInstantPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +37,9 @@ public class ColorSetUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_color_set_up);
         databaseReference = FirebaseDatabase.getInstance().getReference("matchesTry");
         buttonColor = (Button) findViewById(R.id.colorButtonDistributed);
-        buttonInstantPlay=(Button)findViewById(R.id.buttonStartGameDistributed);
+        setColor();
+
+        buttonInstantPlay = (Button) findViewById(R.id.buttonStartGameDistributed);
         buttonInstantPlay.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -57,32 +60,70 @@ public class ColorSetUpActivity extends AppCompatActivity {
             }
         });
 
-        setColor();
 
     }
 
     public void sendColor(View view) throws ExecutionException, InterruptedException {
 
 
+
+    }
+
+    public void onResume() {
+        super.onResume();
+        Log.d("ACTIVITYRESUME", "this resume is called");
+        databaseReference.child(CHILD_PLAYERS).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Log.d("CHILDADDED ",dataSnapshot.getKey()+" "+dataSnapshot.getValue().toString());
+                if(color.equals(dataSnapshot.getValue())){
+                    buttonColor.setText(color+" blink!!!");
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                Log.d("CHILDADDED ",dataSnapshot.getKey()+" "+dataSnapshot.getValue().toString());
+                if(color.equals(dataSnapshot.getValue())){
+                    buttonColor.setText(color+" blink!!!");
+                }
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
     private void setColor() {
         Log.d("PROVA", "executing query");
         databaseReference.child(CHILD_PLAYERS).addListenerForSingleValueEvent(new ValueEventListener() {
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot singleDataSnapshot : dataSnapshot.getChildren()) {
                     HashMap<String, String> player_info = (HashMap<String, String>) singleDataSnapshot.getValue();
                     Log.d("PROVA", player_info.toString());
                     playerID = singleDataSnapshot.getKey();
-
+                    Log.d("PLAYERID", playerID);
 
                     if (!Boolean.parseBoolean(player_info.get("taken"))) {
                         color = player_info.get("color");
 
                         databaseReference.child(CHILD_PLAYERS).child(playerID).child("taken").setValue("true");
-                        buttonColor.setText(playerID+" "+color);
+                        buttonColor.setText(playerID + " " + color);
 
 
                         Log.d("CHILDSNAPSHOT", "changing value");
@@ -100,14 +141,12 @@ public class ColorSetUpActivity extends AppCompatActivity {
         });
     }
 
-    public void startGame(View view){
+    public void startGame(View view) {
 
-        if(view.isPressed()){
+        if (view.isPressed()) {
 
         }
     }
-
-
 
 
 }
